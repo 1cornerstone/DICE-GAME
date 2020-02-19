a5fa4b578af6f0ea60da2253d6a8e9e98da10d04
Akintunde Segun <Akintundeayofesegun@gmail.com>
2020-02-19 06:17:09 -0500

    update home component

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,63 +1,123 @@
 import React, {Component} from 'react'
-import {Grid,Avatar,Typography} from "@material-ui/core";
+import {Grid, Typography, Card, CardContent, Container,Avatar} from "@material-ui/core";
 import background from './assest/diceplay.jpg'
-import dice from './assest/dice.png'
+import register from './assest/register.png'
+import wallet from './assest/wallet.png'
+import multiplayer from './assest/multiplayer.png'
+import computer from './assest/computer.png'
 
 
-class home extends Component{
+class home extends Component {
 
     render() {
 
         const styles = {
-             root : {
-                 flexGrow: 1,
-                 width: '100%',
-             },
-            image:{
-                 width:'150px',
-                height:'100px',
+            root: {
+                flexGrow: 1,
+                width: '100%',
+                marginTop:'20px'
+            },
+            typo: {
+               margin:'10px',
                 textAlign:'center'
 
             },
-            carousel:{
-               backgroundImage: `url(${background})`,
-                width:'100%',  height:'40vh',
-                backgroundSize:'cover',
+            avatar :{
+               fontSize:'20px' ,
+                margin: "auto",
+                width: '40px',
+                height:'40px'
             }
+
         };
 
-            return (
-                <Grid container style={styles.root} spacing={2} direction="row"  >
+        return (
+            <Container>
+                <Grid container style={styles.root} spacing={2} direction="column" justify='center' alignItems='center' >
 
-                    <Grid item  style={styles.carousel}>
-
-                        <Avatar variant="square" alt="dice" src={dice} style={styles.image}  className='frontImage' />
+                    <Grid container direction="row" justify='center' alignItems='center' >
+                        <Grid item ><Avatar style={styles.avatar}>D</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>I</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>C</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>E</Avatar></Grid>
+                        <Grid item style={{margin:'2px'}}></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>G</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>A</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>M</Avatar></Grid>
+                        <Grid item ><Avatar style={styles.avatar}>E</Avatar></Grid>
 
                     </Grid>
-                    <Grid container direction='row'  justify='center' style={{margin:'30px',textAlign:'center'}}>
 
+                    <Grid item>
+                        <Typography variant="h5" component="h5">How it works</Typography>
 
-                            <Typography variant="h4" component="h4">
-                                                 DICE GAME
-                            </Typography>
-                        <Typography variant="h6" component="h6">
-                                Dice games are games that use or incorporate one or more dice as their sole or central component, usually as a random device.
-                            </Typography>
-                            <Typography variant='h6'>
-                                How to play:
-                            </Typography>
+                    </Grid>
+                    <Grid container spacing={2} direction="row" justify='center' alignItems='center'>
 
-                        <ul style={{textAlign:'left',listStyleType:"none", marginTop:'20px'}}>
+                        <Grid item xs={12} sm={3}>
+                            <Card>
+                                <CardContent>
+                                        <Avatar style={{height:'50px', width:'50px', fontSize:'60px',margin:'auto'}} src={register}></Avatar>
 
-                            <li> Register and Sign In,</li>
-                            <li>your will get 500DG once you register to play game.</li>
-                            <li>you can play with computer or play with your friend.</li>
-                        </ul>
+                                    <Typography gutterBottom variant="body1"  component="p"  style={styles.typo}>
+                                       Register
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                       Register content
+                                    </Typography>
+                                </CardContent>
+
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Card>
+                                <CardContent>
+                                    <Avatar style={{height: '50px', width: '50px', fontSize: '60px', margin: 'auto'}} src={wallet}/>
+
+                                    <Typography gutterBottom variant="body1"  component="p" style={styles.typo}>
+                                       TOP UP
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                       Top up content
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Card>
+                                <CardContent>
+                                    <Avatar style={{height:'50px', width:'60px', fontSize:'60px',margin:'auto'}} src={multiplayer}/>
+                                    <Typography gutterBottom variant="body1" component="p">
+                                        Register
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Register content
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Card>
+                                <CardContent>
+                                    <Avatar style={{height:'50px', width:'50px', fontSize:'60px',margin:'auto'}} src={multiplayer}/>
+                                    <Typography gutterBottom variant="body1" component="p">
+                                        Register
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Register content
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+
 
                     </Grid>
 
                 </Grid>
-            );
-        }
+
+            </Container>
+        );
+    }
 }
-export  default  home;
+
+export default home;
