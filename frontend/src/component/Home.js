70408b5b5670ea0aab7a315228e2d3d49fd3cb15
Akintunde Segun <Akintundeayofesegun@gmail.com>
2020-02-18 19:49:54 -0500

    update home design

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,43 +1,58 @@
 import React, {Component} from 'react'
 import {Grid,Avatar,Typography} from "@material-ui/core";
-import diceImage from './assest/dice.png'
+import background from './assest/diceplay.jpg'
+import dice from './assest/dice.png'
 
 
 class home extends Component{
-
-    constructor(props){
-        super(props);
-
-        this.state = {
-            username : ""
-        }
-    }
-
 
     render() {
 
         const styles = {
              root : {
-                 marginTop: '10%',
                  flexGrow: 1,
-                 color:'white',
-                 width: '100%'
+                 width: '100%',
              },
             image:{
-                 width:'200px',
-                height:'140px',
-                margin:'auto'
+                 width:'150px',
+                height:'100px',
+                textAlign:'center'
 
+            },
+            carousel:{
+               backgroundImage: `url(${background})`,
+                width:'100%',  height:'40vh',
+                backgroundSize:'cover',
             }
         };
 
             return (
-                <Grid container spacing={2} style={styles.root} direction="column" justify="center" alignItems="center" >
+                <Grid container style={styles.root} spacing={2} direction="row"  >
 
-                    <Grid item xs={12} sm={12} style={ {backgroundColor:'red',width:'100%'}}>
+                    <Grid item  style={styles.carousel}>
+
+                        <Avatar variant="square" alt="dice" src={dice} style={styles.image}  className='frontImage' />
 
                     </Grid>
-                    <Grid item xs={12} sm={12} style={ {backgroundColor:'black',width:'100%'}}>
+                    <Grid container direction='row'  justify='center' style={{margin:'30px',textAlign:'center'}}>
+
+
+                            <Typography variant="h4" component="h4">
+                                                 DICE GAME
+                            </Typography>
+                        <Typography variant="h6" component="h6">
+                                Dice games are games that use or incorporate one or more dice as their sole or central component, usually as a random device.
+                            </Typography>
+                            <Typography variant='h6'>
+                                How to play:
+                            </Typography>
+
+                        <ul style={{textAlign:'left',listStyleType:"none", marginTop:'20px'}}>
+
+                            <li> Register and Sign In,</li>
+                            <li>your will get 500DG once you register to play game.</li>
+                            <li>you can play with computer or play with your friend.</li>
+                        </ul>
 
                     </Grid>
 
